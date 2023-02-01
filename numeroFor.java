04edbb3031dedccadaeca9a24ad87b77ed09fe93
maxbotero <124061834+maxbotero@users.noreply.github.com>
2023-02-01 09:39:46 -0500

    Create numeroFor.java
    Para el bucle For, crea una variable numeroFor, esta variable tendrá como valor 0 y su condición será que la variable sea igual o menor que 3, se irá incrementando en 1 su valor cada vez que se ejecute y deberá mostrarse por pantalla.

---
+++ numeroFor.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int numeroFor;
+        for (numeroFor = 0;numeroFor <= 3;numeroFor++){
+            System.out.println(numeroFor);
+        }
+
+    }
+
+}
