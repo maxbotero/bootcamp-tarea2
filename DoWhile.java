173e142a09150a077894eb77c912ef908cdb3120
maxbotero <124061834+maxbotero@users.noreply.github.com>
2023-02-01 09:34:13 -0500

    Create DoWhile.java
    Para el bucle Do While, deberás crear la misma estructura que en el While, pero solo se debe ejecutar una vez.

---
+++ DoWhile.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+      
+        int numeroWhile = 3;
+        do {
+            numeroWhile ++;
+            System.out.println(numeroWhile);
+        } while(numeroWhile < 3);
+
+    }
+
+}
